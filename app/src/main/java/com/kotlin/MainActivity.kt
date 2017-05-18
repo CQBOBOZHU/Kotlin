325616f18643539f68bfbcf6a115b727197d30cc
bobozhu <347370269@qq.com>
2017-05-18 17:55:29 +0800

    添加更多

--- app/src/main/java/com/kotlin/MainActivity.kt
+++ app/src/main/java/com/kotlin/MainActivity.kt
@@ -3,22 +3,34 @@ package com.kotlin
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
-import android.widget.LinearLayout
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    var recycler: RecyclerAdapter? = null
+class MainActivity : AppCompatActivity(), RecyclerAdapter.iTemClick {
+    override fun itemClick(position: Int) {
+        Log.v("wamgmo,a ", "dawdawda")
+    }
+
+    var adapter: RecyclerAdapter? = null
     internal var mData: ArrayList<String>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         test_tv.text = "你大爷的hello world"
-        test_btn.setOnClickListener { test_tv.text = "你二大爷的hello world" }
+        test_btn.setOnClickListener {
+            var mList:ArrayList<String> = ArrayList<String>()
+            for (i in 1..50){
+                mList.add("新加数据"+i)
+            }
+            adapter!!.addNewData(mList)
+
+        }
         initData()
-        recycler = RecyclerAdapter(this, mData!!)
-        recyclerView.layoutManager= LinearLayoutManager(this)
-        recyclerView.adapter = recycler
+        adapter = RecyclerAdapter(this, mData!!)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+        adapter!!.setITemClick(this)
 
     }
 
@@ -31,7 +43,6 @@ class MainActivity : AppCompatActivity() {
 
 
     fun initView(savedInstanceState: Bundle?) {
-        Toast.makeText(this, "adada", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
--- app/src/main/java/com/kotlin/RecyclerAdapter.kt
+++ app/src/main/java/com/kotlin/RecyclerAdapter.kt
@@ -3,17 +3,14 @@ package com.kotlin
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
 
-import java.util.ArrayList
 
 /**
  * Created by Administrator on 2017/5/18.
  */
 
-class RecyclerAdapter(internal var context: Context, internal var mData: List<String>) : RecyclerView.Adapter<ViewHolder>() {
+class RecyclerAdapter(internal var context: Context, internal var mData: ArrayList<String>) : RecyclerView.Adapter<ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.recycler_item, parent, false)
@@ -23,9 +20,29 @@ class RecyclerAdapter(internal var context: Context, internal var mData: List<St
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.textView.text = mData[position]
+        holder.itemView.setOnClickListener {
+            itemClick!!.itemClick(position)
+        }
     }
 
     override fun getItemCount(): Int {
         return mData.size
+    }
+
+    var itemClick: iTemClick? = null
+
+    fun setITemClick(itemClick: iTemClick) {
+        this.itemClick = itemClick
+    }
+
+    fun addNewData(mList:List<String>){
+        for (i in 0..mList.size-1){
+            mData!!.add(mList.get(i))
+            notifyItemInserted(itemCount)
+        }
+    }
+
+    interface iTemClick {
+        fun itemClick(position: Int)
     }
 }
